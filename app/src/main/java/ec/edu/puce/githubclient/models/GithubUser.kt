f9f7c84ae14e657d6f5a02540f388f5a77595000
jxherrera <josuexavi728@gmail.com>
2026-05-28 22:18:27 -0500

    corrigiendo errores

--- app/src/main/java/ec/edu/puce/githubclient/models/GithubUser.kt
+++ app/src/main/java/ec/edu/puce/githubclient/models/GithubUser.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class GithubUser (
     val id: String,
     val login: String,
-    @SerializedName(value = "avatar_Url")
+    @SerializedName(value = "avatar_url")
     val avatarUrl: String
 
 )
